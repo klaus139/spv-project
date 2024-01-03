c646d22df627f70e53b823da0efb0f90ee44b1db
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2024-01-03 09:23:30 +0100

    the spv website is getting started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider, Outlet} from 'react-router-dom'
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import Menu from './components/Menu/Menu';
+
+const Layout = () => {
+  const[clicked,isClicked] = useState(false)
+  return (
+    <div className='app'>
+  <Navbar clicked={clicked} isClicked={isClicked}/>
+  {clicked?<Menu/> :null}
+    <Outlet />
+    <Footer/>  
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      
+    ]
+  },
+  
+
+  
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
+     
     </div>
   );
 }
+
 
 export default App;
---
+++ src/constants/images.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+import logo from '../assets/WhatsApp_Image_2023-12-21_at_7.03.22_AM-removebg-preview.png';
+
+
+export default {
+    logo,
+}
